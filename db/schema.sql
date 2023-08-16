749ca4625d0a4b12284326889ca1087ea3c29986
Cecilj24 <clangba@yahoo.com>
2023-08-16 12:25:43 -0700

    finished cms

--- db/schema.sql
+++ db/schema.sql
@@ -9,11 +9,7 @@ CREATE TABLE department(
 );
 SHOW TABLES;
 
-INSERT INTO department(name)
-VALUES ("Finance"),
-("Lead Engineer"),
-("HR"),
-("Legal");
+
 SELECT * FROM department;
 
 CREATE TABLE role(
@@ -33,17 +29,33 @@ CREATE TABLE employee(
 SHOW TABLES;
 
 INSERT INTO department(name)
-VALUES ("Sales");
+VALUES ("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
 
-INSERT INTO role(title)
-VALUES ("Sales Lead"),
-("Salesperson"),
-("Lead Engineer"),
-("Software Engineer"),
-("Account Manager"),
-("Accountant"),
-("Legal Team Lead");
+SELECT * from role;
 
-SHOW TABLES;
+INSERT INTO role(title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1),
+("Sales Person", 90000, 1),
+("Lead Engineer",150000, 2),
+("Software Engineer",130000, 2),
+("Account Manager",160000, 3),
+("Accountant",130000, 3),
+("Legal Team Lead",250000, 4),
+("Lawyer",180000, 4);
 
-SELECT * FROM role;
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Cecil", "John", 2, 1),
+("Jacob", "Smith", 1, 1),
+("Jonathan", "Doe", 1,2),
+("Monica", "Sanchez", 2,2),
+("Tom", "Leonard", 3,3),
+("Devin", "Thomas", 3,3),
+("Ashley", "Rodriguez", 4,4),
+("Kevin", "Johnson", 4,4);
+
+SELECT * from employee;
+
+UPDATE employee SET employee;
--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // TODO: Add MySQL password here
-      password: 'root',
-      database: 'employee_db'
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // TODO: Add MySQL password here
+        password: 'root',
+        database: 'employee_db'
     },
     console.log(`Connected to the movies_db database.`)
-  );
+);
 
-  function init () {
+function init() {
     inquirer.prompt([
         {
             type: "list",
@@ -25,48 +25,60 @@ const db = mysql.createConnection(
                 "View all employees?",
                 "Add department?",
                 "Add a role?",
-                "Add an employee?"
+                "Add an employee?",
+                "Update employee role?"
             ]
         }
-    ]).then((data)=>{
+    ]).then((data) => {
         console.log(data)
         // create if statement here
-        if(data.Userchoice === "View all departments?"){
-                ViewDepartments()
+        if (data.Userchoice === "View all departments?") {
+            ViewDepartments()
         }
-        if(data.Userchoice === "View all roles?"){
-
+        if (data.Userchoice === "View all roles?") {
+            ViewRoles()
         }
-        if(data.Userchoice === "View all employees?"){
-
+        if (data.Userchoice === "View all employees?") {
+            ViewEmployees()
         }
-        if(data.Userchoice === "Add department?"){
+        if (data.Userchoice === "Add department?") {
             addDepartment()
         }
-        if(data.Userchoice === "Add a role?") {
-
+        if (data.Userchoice === "Add a role?") {
+                addRole ()
         }
-
+        if (data.Userchoice === "Add an employee?") {
+            addEmployee ()
+        }
+        if (data.Userchoice ===  "Update employee role?") {
+                updateEmployee ()
+        }
     })
-  }
-  init() 
+}
+init()
 
 
-  // create a function that goes with the "if" statement (refer back to miniproject if you get stuck)
-  // query tells what to do to the database
-  function ViewDepartments() {
-    db.query("SELECT * FROM department", (err, data) =>{
+// create a function that goes with the "if" statement (refer back to miniproject if you get stuck)
+// query tells what to do to the database
+function ViewDepartments() {
+    db.query("SELECT * FROM department", (err, data) => {
         console.table(data)
     })
-  }
+}
 
-  function ViewRoles () {
-    db.query ("SELECT * from role" , (err, data)=>{
+function ViewRoles() {
+    db.query("SELECT * from role", (err, data) => {
         console.table(data)
     })
-  }
+}
 
-  function addDepartment () {
+function ViewEmployees() {
+    db.query("SELECT * FROM employee", (err, data) => {
+        console.table(data)
+    })
+}
+
+function addDepartment() {
     inquirer.prompt([
         {
             type: "input",
@@ -80,4 +92,93 @@ const db = mysql.createConnection(
             init()
         })
     })
-  }
+}
+
+function addRole() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of the role?", 
+             name: "Newrole"
+        },
+        {
+            type: "input",
+            message: "What is the salary of the role",
+            name: "salary"
+        },
+        {
+            type: "input",
+            message: "What is the department id of this role",
+            name: "departmentid"
+        }
+    ]).then((data) => {
+        db.query(`INSERT INTO role (title, salary, department_id) VALUES(?,?,?)`, [data.Newrole, data.salary, data.departmentid], (err, res) => {
+            if (err) throw err
+            console.log("New role added successfully!")
+            init()
+        })
+    })
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of the employee?", 
+             name: "firstname"
+        },
+        {
+            type: "input",
+            message: "What is the salary of the employee?",
+            name: "salary"
+        },
+        {
+            type: "input",
+            message: "What is the department id of the employee?",
+            name: "departmentid"
+        },
+        {
+                type: "input",
+                message: "What is the manager id of the employee?",
+                name: "managerid"
+            
+        }
+    ]).then((data) => {
+        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)`, [data.firstname, data.salary, data.departmentid, data.managerid], (err, res) => {
+            if (err) throw err
+            console.log("New employee added successfully!")
+            init()
+        })
+    })
+}
+
+function updateEmployee () {
+   inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the first name of the employee?",
+        name: "firstname"
+    },
+    {
+        type: "input",
+        message: "What is the last name of the employee?",
+        name: "lastname"
+    },
+    {
+        type: "input",
+        message: "What is the employees role id",
+        name: "roleid"
+    },
+    {
+        type: "input",
+        message: "What is the employees manager id?",
+        name: "managerid"
+    }
+   ]).then((data)=>{
+    db.query(`UPDATE employee SET first_name = ?`,`UPDATE employee SET last_name = ?`,`UPDATE employee SET role_id = ?`, `UPDATE employee SET manager_id = ?`, [data.firstname, data.lastname, data.roleid, data.managerid], (err, res) => {
+        if (err) throw err
+            console.log("employee updated successfully!")
+            init()
+    })
+   })
+}
